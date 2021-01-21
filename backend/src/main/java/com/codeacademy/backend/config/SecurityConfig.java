49b2139c5ae6ed36de2b8804b1a978caf0515339
David Bezprozvani <david.bezprozvani@gmail.com>
2021-01-21 20:10:03 +0200

    added security configuration

--- backend/src/main/java/com/codeacademy/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/codeacademy/backend/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.codeacademy.backend.config;
 
+import com.codeacademy.backend.security.JWTAuthenticationFilter;
+import com.codeacademy.backend.security.JwtProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,6 +15,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager(), new JwtProvider()));
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
